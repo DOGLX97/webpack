2d01a38732927225f3a79a9331ab9666e91ac629
DOGLX97 <15663423507@163.com>
2017-10-27 15:54:52 +0800

    webpack

--- webpack/dist/entry.js
+++ webpack/dist/entry.js
@@ -65,11 +65,22 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_index_css__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_index_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__css_index_css__);
 
 let oDiv=document.createElement("div");
 oDiv.innerHTML='hello world123！';
 document.body.appendChild(oDiv);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
